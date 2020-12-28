8da56cae40de53ffe0117981cafdbe3ff1f885d6
Yahav N Hoffman <73889722+yahav-h@users.noreply.github.com>
2020-12-28 12:50:40 +0200

    Update appdescription.py

--- spoofer/utils/appdescription.py
+++ spoofer/utils/appdescription.py
@@ -2,7 +2,7 @@ from . import logger
 
 description = """  email-spoofer-py v0.0.3 (CLI wizard)  
   Python 3.x based email spoofer
-  https://github.com/mikechabot/email-spoofer-py"""
+  https://github.com/yahav-h/smtp_email_spoofer""
 
 def print_description():
     logger.bright('\n{0}'.format('='*50))
