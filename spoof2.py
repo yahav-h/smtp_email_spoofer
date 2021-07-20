ea5019dc9badd194a035d1a2b651cad68ace8df8
Yahav-N-Hoffman <yahavh@avanan.com>
2021-07-20 12:25:33 +0300

    update spoof2

--- spoof2.py
+++ spoof2.py
@@ -1,7 +1,8 @@
 from spoofer.utils.config import Config
 from spoofer.models.smtpconnection import SMTPConnection, cout
+from concurrent.futures import ThreadPoolExecutor, FIRST_EXCEPTION, wait
 import json
-
+import datetime
 
 global collected_ids
 collected_ids = []
@@ -41,34 +42,40 @@ def threaded_main():
         senders.append(Sender(s['inbox'], s['pwd']))
 
     def thread(sender, data):
-        global collected_ids
-        counter = 0
-        with open(f'{Config.get_templates()}/{data.body}', 'r') as fin:
-            html = fin.read()
-            html.replace('{{userName}}', ', '.join(data.recipients))
-        while counter < data.max:
-            conn = SMTPConnection(data.host, data.port)
-            conn.login(sender.email, sender.pwd)
-            msg = conn.compose_message(
-                sender=sender.email,
-                name=sender.email.split('@')[0],
-                recipients=data.recipients,
-                subject=data.subject,
-                html=html,
-                headers='',
-                attachments=data.attachments,
-                withUUID=0,
-                isCC=0
-            )
-            hex_id = msg['Message-ID']
-            msg['Subject'] = msg['Subject'] + str(hex_id)
-            conn.send_mail(msg)
-            cout.success(f'Sender {sender.email} sent message {hex_id} to {", ".join(data.recipients)}')
-            conn.quit()
-            collected_ids.append(hex_id)
-            counter += 1
+        try:
+            global collected_ids
+            counter = 0
+            with open(f'{Config.get_templates()}/{data.body}', 'r') as fin:
+                html = fin.read()
+                html = html.replace('{{userName}}', ', '.join(data.recipients))
+            subjects = []
+            while counter < data.max:
+                conn = SMTPConnection(data.host, data.port)
+                conn.login(sender.email, sender.pwd)
+                msg = conn.compose_message(
+                    sender=sender.email,
+                    name=sender.email.split('@')[0],
+                    recipients=data.recipients,
+                    subject=data.subject,
+                    html=html,
+                    headers='',
+                    attachments=data.attachments,
+                    withUUID=0,
+                    isCC=0
+                )
+                hex_id = msg['Message-ID']
+                suffix = str(datetime.datetime.now().isoformat()) + " " + str(hex_id)
+                msg['Subject'] += suffix
+                subjects.append(msg['Subject'] + suffix)
+                conn.send_mail(msg)
+                cout.success(f'Sender {sender.email} sent message {hex_id} to {", ".join(data.recipients)}')
+                conn.quit()
+                collected_ids.append(hex_id)
+                counter += 1
+            return True, subjects
+        except:
+            return False, None
 
-    from concurrent.futures import ThreadPoolExecutor, FIRST_EXCEPTION, wait
     with ThreadPoolExecutor(max_workers=len(senders)) as execs:
         fs = []
         futures = []
