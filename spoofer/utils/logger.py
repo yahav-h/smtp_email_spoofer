e0433135e3c83ee8446ac2de45390dd605a1ec61
Yahav-N-Hoffman <yahavh@avanan.com>
2021-07-20 12:26:09 +0300

    update logger

--- spoofer/utils/logger.py
+++ spoofer/utils/logger.py
@@ -4,20 +4,20 @@ init()
 
 
 def header(line):
-    print(Fore.LIGHTBLACK_EX + line + Style.RESET_ALL)
+    print(Fore.LIGHTBLACK_EX + str(line) + Style.RESET_ALL)
 
 
 def info(line):
-    print(Fore.LIGHTCYAN_EX + line + Style.RESET_ALL)
+    print(Fore.LIGHTCYAN_EX + str(line) + Style.RESET_ALL)
 
 
 def success(line):
-    print(Fore.LIGHTGREEN_EX + line + Style.RESET_ALL)
+    print(Fore.LIGHTGREEN_EX + str(line) + Style.RESET_ALL)
 
 
 def error(line):
-    print(Fore.LIGHTRED_EX + line + Style.RESET_ALL)
+    print(Fore.LIGHTRED_EX + str(line) + Style.RESET_ALL)
 
 
 def bright(line):
-    print(Fore.LIGHTMAGENTA_EX + line + Style.RESET_ALL)
+    print(Fore.LIGHTMAGENTA_EX + str(line) + Style.RESET_ALL)
